228b4976e3e855a70a64fb2ab5e7fa4d4799ac48
jose.chaves <jechaves@gmail.com>
2018-02-18 23:42:15 +0000

    Nova nav bar

---
+++ js/loops.js
@@ -0,0 +1,14 @@
+//while
+console.log('Testes while');
+document.getElementById("rodapeWhile").innerHTML += "teste idiota";
+a1 = 1;
+console.log('Valor inicial de a = ' + a1);
+while (a1++ < 5)
+{
+    console.log('Valor de a1 quando entra no while = ' + a1);
+    a1++;
+    console.log('Valor de a1 quando sai do while = ' + a1);
+}
+console.log('Valor do a1 quando termina o while = ' + a1);
+
+console.log('-----------------------------------');
