d10353dc02f68165ecef27e86a6fd872b55069b6
jose.chaves <jechaves@gmail.com>
2018-02-25 15:53:46 +0000

    New restyle

--- js/loops.js
+++ js/loops.js
@@ -1,6 +1,6 @@
 //while
 console.log('Testes while');
-document.getElementById("rodapeWhile").innerHTML += "teste idiota";
+
 a1 = 1;
 console.log('Valor inicial de a = ' + a1);
 while (a1++ < 5)
@@ -12,3 +12,4 @@ while (a1++ < 5)
 console.log('Valor do a1 quando termina o while = ' + a1);
 
 console.log('-----------------------------------');
+document.getElementById("rodapeWhile").innerHTML += "teste idiota";
